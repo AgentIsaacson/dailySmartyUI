a2c5ef3cd620033ce21e8378e0033d850cade04d
Araliatrix <agentisaacson@gmail.com>
2018-08-01 14:03:50 -0600

    set up logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Logo from "./logo";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>DevCamp React Starter</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Logo/>
+			</div>
+		);
+	}
 }
